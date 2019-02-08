6379c03955d74c39c33d7a2dbd1a6890abb5ca17
Ralf Wunsch <r.wunsch@ist-software.com>
2019-02-08 17:04:45 +0100

    simple workflow module - first ideas

---
+++ bundle/src/main/java/com/composum/platform/tenant/workflow/onboarding/AssignRole.java
@@ -0,0 +1,41 @@
+package com.composum.platform.tenant.workflow.onboarding;
+
+import com.composum.platform.tenant.service.TenantManagerService;
+import com.composum.platform.workflow.service.WorkflowService;
+import com.composum.platform.workflow.service.WorkflowTask;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+        property = {
+                JobConsumer.PROPERTY_TOPICS + "=" + AssignRole.JOB_TOPIC
+        }
+)
+public class AssignRole implements JobConsumer {
+
+    public static final String JOB_TOPIC = "platform.tenant.workflow.onboarding.assign-role";
+
+    @Reference
+    private ResourceResolverFactory resolverFactory;
+
+    @Reference
+    private WorkflowService workflowService;
+
+    @Reference
+    private TenantManagerService tenantManager;
+
+    @Override
+    public JobResult process(Job job) {
+        try (ResourceResolver jobResolver = resolverFactory.getServiceResourceResolver(null)) {
+            WorkflowTask task = workflowService.getTask(jobResolver, job);
+
+            return JobResult.OK;
+        } catch (Exception ex) {
+            return JobResult.FAILED;
+        }
+    }
+}
